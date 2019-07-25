bfdec95376407dba7f90fc1fd5ca38bf2aeafb9e
Michael Pohily <mpohily@gmail.com>
2019-07-25 12:54:44 +0000

    add purchse view

--- purse_app/views.py
+++ purse_app/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
 
+from .models import *
+
 
 def purse_app_start(request):
     return render(request, 'purse_app/start.html', {})
+
+
+def income(request):
+    return render(request, 'purse_app/income.html', {})
+
+
+def purchase(request):
+    purchases = Purchase.objects.all()[:10]
+    return render(request, 'purse_app/purchase.html', {'purchases': purchases})
+
+
+def debit(request):
+    return render(request, 'purse_app/debit.html', {})
+gi
+
+def credit(request):
+    return render(request, 'purse_app/credit.html', {})
+
+
+def stats(request):
+    return render(request, 'purse_app/stats.html', {})

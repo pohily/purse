75e4ab86bf39ff4b3261e56978d4c2d52171740c
Michael Pohily <mpohily@gmail.com>
2019-07-27 13:08:34 +0000

    add credit and deposit models. change date from now to localdate

--- purse_app/models.py
+++ purse_app/models.py
@@ -12,10 +12,14 @@ class Summary(models.Model):
 
 class Purchase(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    date = models.DateTimeField(default=timezone.now)
+    date = models.DateField(default=timezone.localdate)
     budget_line = models.ForeignKey('PurchaseBudgetLine', to_field='line', on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    comment = models.CharField(max_length=200)
+    comment = models.CharField(max_length=100)
+    with_credit_card = models.ForeignKey(
+        'CreditCards', to_field='name', null=True, blank=True, on_delete=models.SET_NULL
+    )
+    with_debit_card = models.ForeignKey('DebitCards', to_field='name', null=True, blank=True, on_delete=models.SET_NULL)
 
     def submit(self):
         if not self.date:
@@ -23,15 +27,16 @@ class Purchase(models.Model):
         self.save()
 
     def __str__(self):
-        return f'{self.date} {self.amount} {self.budget_line} {self.comment[:20]} ...'
+        return f'{self.date} {self.amount} {self.budget_line} {self.comment}'
 
 
 class Income(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    date = models.DateTimeField(default=timezone.now)
+    date = models.DateField(default=timezone.localdate)
     budget_line = models.ForeignKey('IncomeBudgetLine', to_field='line', on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    comment = models.CharField(max_length=200)
+    comment = models.CharField(max_length=100)
+    to_debit_card = models.ForeignKey('DebitCards', to_field='name', null=True, blank=True, on_delete=models.SET_NULL)
 
     def submit(self):
         if not self.date:
@@ -39,7 +44,7 @@ class Income(models.Model):
         self.save()
 
     def __str__(self):
-        return f'{self.date} {self.amount} {self.budget_line} {self.comment[:20]} ...'
+        return f'{self.date} {self.amount} {self.budget_line} {self.comment}'
 
 
 class PurchaseBudgetLine(models.Model):
@@ -54,3 +59,38 @@ class IncomeBudgetLine(models.Model):
 
     def __str__(self):
         return f'{self.line}'
+
+
+class DebitCards(models.Model):
+    name = models.CharField(max_length=40, unique=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class OtherDebits(models.Model):
+    name = models.CharField(max_length=40, unique=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.name} {self.amount}'
+
+
+class CreditCards(models.Model):
+    name = models.CharField(max_length=40, unique=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    grace_period_end = models.DateField(default=timezone.localdate)
+    comment = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class OtherCredits(models.Model):
+    name = models.CharField(max_length=40, unique=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    comment = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.name} {self.amount}'
